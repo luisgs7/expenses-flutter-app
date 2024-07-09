81357b207b5de5accd864e5ed954ba68352ef6f0
luisgs7 <luisgsilva270@gmail.com>
2024-07-08 21:57:00 -0300

    use column

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,30 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Despesas Pessoais"),
-      ),
-      body: Center(
-        child: Text("Versão Inicial"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Despesas Pessoais"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text(
+                  "Gráfico",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              child: Text(
+                'Lista de Transações.',
+              ),
+            )
+          ],
+        ));
   }
 }
