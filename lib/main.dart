a486dbd1ad3d66e9a2b3125f04387c39f9709a4c
luisgs7 <luisgsilva270@gmail.com>
2024-07-08 21:46:29 -0300

    start project

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Despesas Pessoais"),
+      ),
+      body: Center(
+        child: Text("Versão Inicial"),
+      ),
+    );
+  }
+}
