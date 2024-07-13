8f59981179b5614ad966d9e1d81f47ce9327c135
luisgs7 <luisgsilva270@gmail.com>
2024-07-13 16:03:21 -0300

    refctor app and created nem components.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:expenses/models/transaction.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -16,82 +16,23 @@ class ExpensesApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   // const MyHomePage({super.key});
 
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Iphone 15',
-      value: 310.78,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Notebook',
-      value: 5000.00,
-      date: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Despesas Pessoais"),
+      appBar: AppBar(
+        title: const Text(
+          "Despesas Pessoais",
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text(
-                  "Gráfico",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                elevation: 5,
-              ),
-            ),
-            Column(
-              children: _transactions.map((tr) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          tr.value.toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          Text(tr.title),
-                          Text(tr.date.toString()),
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            )
-          ],
-        ));
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          TransactionUser(),
+        ],
+      ),
+    );
   }
 }
