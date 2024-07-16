d75ad5ff985a1f303c3deac0ae0d2ac20100cbe3
luisgs7 <luisgsilva270@gmail.com>
2024-07-15 22:01:56 -0300

    adicionando um valor a lista.

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -6,6 +6,10 @@ class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -31,8 +35,10 @@ class TransactionForm extends StatelessWidget {
               children: [
                 TextButton(
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0;
+
+                    onSubmit(title, value);
                   },
                   style: ButtonStyle(
                     foregroundColor: MaterialStateProperty.all(
