5d231cc6ce3752060060634b73021ce4e65998e8
luisgs7 <luisgsilva270@gmail.com>
2024-07-30 21:15:34 -0300

    use chartBar

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/components/chart_bar.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -45,7 +46,11 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupedTransactions.map((tr) {
-          return Text('${tr['day']}: ${tr['value']}');
+          return ChartBar(
+            label: tr['day'],
+            value: tr['value'],
+            percentage: 0,
+          );
         }).toList(),
       ),
     );
